2be8e800cf3ef16377c2e47b179fb1127412fa7d
tangentstorm <michal.wallace@gmail.com>
2012-11-12 23:04:04 -0600

    updated xpc.hex test to the new style

--- test/test_xpc.pas
+++ test/test_xpc.pas
@@ -1,8 +1,9 @@
-{$i xpc.inc }
-program test_xpc;
-  uses xpc, math;
-  var x : integer;
-begin
-  if hex(1234) = ansistring('4D2') then writeln('ok.')
-  else writeln( 'no: hex says hex(1234) = "', hex(1234), '". Should be "4D2"' );
+{$i test_xpc.def }
+implementation uses xpc;
+
+  procedure test_hex;
+  begin
+    chk.equal( hex( 1234 ), '4D2' );
+  end;
+
 end.
