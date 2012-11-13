264cb481de19c98a468efc829cea12544f7460c9
tangentstorm <michal.wallace@gmail.com>
2012-11-12 23:42:19 -0600

    test_ll : switched to the new style, expanded the test for append

--- test/test_ll.pas
+++ test/test_ll.pas
@@ -1,27 +1,31 @@
 {$i test_ll.def }
-implementation uses ll, sysutils;
+implementation uses ll;
 
   var
     ls	 : list;
-    a, b : node;
+    a, b : pnode;
 
   procedure setup;
   begin
     ls := list.init;
+    new( a ); new( b );
   end;
 
   procedure test_init;
   begin
-    if ls.first <> nil then
-      raise Exception.create( 'expected first = nil' );
-    if ls.last <> nil then
-      raise Exception.create( 'expected last = nil' );
+    chk.that( ls.first = nil, '.first should = nil for empty list' );
+    chk.that( ls.last = nil, '.last should = nil for empty list' );
   end;
 
   procedure test_append;
   begin
-    ls.append( a ); ls.append( b );
-    chk.that( ls.first = b );
+    ls.append( a );
+    chk.that( ls.first = a, 'append to empty should set first' );
+    chk.that( ls.last = a, 'append to empty should set last' );
+    ls.append( b );
+    chk.that( ls.first = a,
+	     'appending second item should not change .first ' );
+    chk.that( ls.last = b, 'appending second item should change last' );
   end;
 
 end.
