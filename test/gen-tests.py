bf223570075e838eb69ca5977d79ff684a2e172c
tangentstorm <michal.wallace@gmail.com>
2012-11-12 23:08:14 -0600

    test framework : show test names and exception messages

--- test/gen-tests.py
+++ test/gen-tests.py
@@ -7,15 +7,21 @@ import os
 topuse = open( 'run-tests.use', 'w' )
 toprun = open( 'run-tests.run', 'w' )
 for path in map( str.strip, os.popen( 'ls test_*.pas' )):
-    name, _ = path.split( '.' )
-    print >> topuse, ',', name ,
-    subdef = open( name + '.def', 'w' )
-    subdef.write( 'unit {0};\ninterface\n'.format( name ))
-    for line in map( str.strip, open( path )):
+    unit_name, _ = path.split( '.' )
+    print >> topuse, ',', unit_name ,
+    subdef = open( unit_name + '.def', 'w' )
+    subdef.write( 'unit {0};\ninterface uses chk;\n'.format( unit_name ))
+    has_setup = False
+    for line in map( lambda s : s.lower().strip(), open( path )):
+        print >> toprun, "unit_name := '%s';" % unit_name
+        if line.startswith( 'procedure setup' ):
+            has_setup = True
         if line.startswith( 'procedure test_' ):
             print >> subdef, ' ', line
-            print >> toprun, 'run( @%s.%s );' \
-                % ( name, line.split(' ', 1 )[ 1 ][ : -1 ]) # strip ";"
+            test_name = line.split( ' ', 1 )[ 1 ]
+            test_name = test_name[ : -1 ] # strip final ";"
+            print >> toprun, "run( '%s', @%s.%s );" \
+                % ( test_name, unit_name, test_name )
     subdef.close()
 topuse.close()
 toprun.close()
--- test/run-tests.pas
+++ test/run-tests.pas
@@ -1,47 +1,85 @@
 {$i xpc.inc}
 program run_tests;
-  uses cw, xpc, sysutils {$i run-tests.use }; // includes test_*.pas
+uses cw, xpc, sysutils {$i run-tests.use }; // includes test_*.pas
+
+type
+  outcome  = ( pass, fail, err );
+  problem  = record
+	       test_name : string;
+	       result	 : outcome;
+	       error	 : string;
+	     end;
+  testcase = procedure;
+const
+  colors : array[ outcome ] of string[ 2 ] = ( '|g', '|y', '|r' );
 
-  type
-    outcome = ( pass, fail, err );
-    testcase = procedure;
-  var
-    overall : outcome;
-    passed, failed, broken  : integer;
+var
+  overall   : outcome;
+  passed,
+  failed,
+  broken    : integer;
+  unit_name : string;
+  problems  : array of problem;
+
+procedure setup;
+begin
+  setlength( problems, 0 );
+  writeln( 'running tests' );
+  writeln;
+end;
 
-  procedure setup;
-  begin
-    writeln( 'running tests' );
-  end;
+
 
-  procedure run( to_test : testcase );
-    var result : outcome = pass;
-  begin
-    // chk.reset;
-    try to_test except
-      on Exception do result := err
+procedure run( test_name : string; to_test : testcase );
+var result : outcome = pass;  p : problem;  e : exception;
+begin
+  //  chk.reset;
+  try to_test
+  except
+    on Exception do begin
+      e := ExceptObject as exception;
+      if e is EAssertionFailed then result := fail
+      else result := err;
+      // setlength( p.error,
+      //   ExceptionErrorMessage( e, ExceptAddr, @p.error[1], 255 ));
+      p.error := e.message;
+      p.test_name := test_name;
+      p.result := result;
+      setlength( problems, length( problems ) + 1 );
+      problems[ length( problems ) - 1 ] := p;
     end;
-    case result of
-      pass : begin inc( passed ); cwrite( '|go' ) end;
-      fail : begin inc( failed ); cwrite( '|y!' ) end;
-      err  : begin inc( broken ); cwrite( '|rx' ) end;
-    end;
   end;
-
-  procedure report;
-    var total : integer;
-  begin
-    cwriteln( '|K ');
-    writeln;
-    cwriteln('----------------------------------------------|w' );
-    writeln;
-    total := passed + failed + broken;
-    writeln( total, ' tests run.' );
-    writeln( passed, ' tests passed; ',
-             failed, ' failed; ',
-             broken, ' broken.' );
+  case result of
+    pass : begin inc( passed ); {c}write( {'|g',} 'o' ) end;
+    fail : begin inc( failed ); {c}write( {'|y',} '!' ) end;
+    err  : begin inc( broken ); {c}write( {'|r',} 'X' ) end;
   end;
+end;
+
+procedure report;
+  var total : integer; p : problem;
+begin
+  //cwriteln( '|K');
+  writeln;
+  {c}writeln('----------------------------------------------' );
+  writeln;
+  for p in problems do begin
+    {c}writeln( {'|K',} '[ ',  { colors[ p.result ] + }
+	       p.test_name, {'|K',}  ' ] ');
+    writeln( {'|w',} p.error );
+    writeln;
+  end;
+  //cwriteln( '|K');
+  {c}writeln('----------------------------------------------' );
+  total := passed + failed + broken;
+  {c}writeln( {'|K',} 'results: ', {'\w',} total, ' tests run. ',
+	   passed, ' tests passed; ',
+           failed, ' failed; ',
+           broken, ' broken.' );
+  writeln;
+end;
 
+
 begin
   setup;
   {$i run-tests.run }
