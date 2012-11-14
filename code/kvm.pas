e46a3248a2ca5b28dd274f0a41573d1a466c960b
tangentstorm <michal.wallace@gmail.com>
2012-11-13 23:12:50 -0600

    kvm: pulled in alt/shift key stubs, enterpressed/getenter from old crtstuff module

--- code/kvm.pas
+++ code/kvm.pas
@@ -6,6 +6,9 @@
 unit kvm;
 interface uses xpc, keyboard;
 
+  procedure getenter;			
+
+  
 {  this should probably get moved into its own class? }
 type
   vector2d = record
@@ -83,6 +86,36 @@ function mx : int32;
 function my : int32;
 function mb : set32;
 
+
+  function alt2normal( ch : Char ) : Char;
+  function shiftstate : Byte;		
+  function enterpressed : Boolean;
+
+
+
+
+
+  const    { altkeys }
+    {  these alt keys constants should move to the
+      kvm unit, and you should realy only deal with
+      some kind of abstraction. }
+    alt16to25 : string = 'QWERTYUIOP';
+    alt30to38 : string = 'ASDFGHJKL';
+    alt44to50 : string = 'ZXCVBNM';
+    
+    { keyboard shift states }
+    rshiftpressed = $01;
+    lshiftpressed = $02;
+    shiftpressed  = $03;
+    ctrlpressed	  = $04;
+    altpressed	  = $08;
+    scrolllockon  = $10;
+    numlockon	  = $20;
+    capslockon	  = $40;
+    inserton	  = $80;
+
+
+  
 implementation
 
 { -- implementation > mouse ------------------------------ }
@@ -230,6 +263,57 @@ procedure bg( b : byte );
 begin
   xterm_bg( b );
 end; { fg }
+
+
+  function alt2normal( ch : Char ) : Char;
+    const
+      set1 = 'QWERTYUIOP';
+      set2 = 'ASDFGHJKL';
+      set3 = 'ZXCVBNM';
+      set4 = '1234567890-=';
+  begin
+    case ord( ch ) of
+      16  ..  25 : alt2normal := set1[ ord( ch ) - 15 ];
+      30  ..  38 : alt2normal := set2[ ord( ch ) - 29 ];
+      44  ..  50 : alt2normal := set3[ ord( ch ) - 43 ];
+      120 .. 131 : alt2normal := set4[ ord( ch ) - 119 ];
+      otherwise
+        alt2normal := #255
+    end { case }
+  end;
+
+
+  //   -- sdl keypress event?
+  function shiftstate : Byte;
+    // var   rgs : registers;
+  begin
+    result := 0;
+    {
+      rgs.AH := $02;
+      intr( $16, rgs );
+      shiftstate := rgs.al;
+      }
+  end; { shiftstate }
+
+
+  function enterpressed : Boolean;
+    var
+      ch : Char;
+  begin
+    ch := ' ';
+    if KeyPressed then begin
+      ch := Readkey;
+    end;
+    enterPressed := ch = #13;
+  end; { enterpressed }
+
+
+  procedure getEnter;
+  begin
+    repeat
+    until enterPressed;
+  end; { getEnter }
+
   
 
 initialization
