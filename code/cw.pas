6c683905d7f92f383181df28253e8856ab64b794
tangentstorm <michal.wallace@gmail.com>
2012-11-20 19:02:32 -0600

    cw: reindent

--- code/cw.pas
+++ code/cw.pas
@@ -2,6 +2,8 @@
 unit cw; { colorwrite }
 interface uses xpc, crt, num, stri;
 
+  const trg = '|'; // trigger char
+
   const
     { cwrite commands }
     cwnotask	 = $00;
@@ -31,23 +33,21 @@ interface uses xpc, crt, num, stri;
   var
     tColor,                        { CWrite color }
     sColor,                        { saved tColor }
-    tXpos, tYpos,                  { CWrite xy pos }
     tXmin, tYmin,                  { CWrite xy min }
-    tXmax, tYmax : byte;           { CWrite xy max }
-    tXSav, tYSav : byte;           { saved t[xy]pos }
+    tXmax, tYmax       : byte;           { CWrite xy max }
+    tXSav, tYSav       : byte;           { saved t[xy]pos }
     cwcommandmode      : boolean;  { cwrite command mode? }
     cwcurrenttask      : byte;     { cwrite current task? }
     cwnchexpected      : byte;     { cwrite #chars expected }
     cwchar,                        { cwrite character }
     cwdigit1, cwdigit2,            { 2nd digit of n1 }
     cwdigit3, cwdigit4 : char;     { 2nd digit of n2 }
-
+		       
 { ■ string writing commands }
-
+		       
   { primitives	       : these write text in solid colors }
   procedure colorxy  ( const x, y, c : byte; const s : string );
   procedure colorxyc ( x, y, c : byte; s : string );
-
   procedure colorxyv ( const x, y, c : byte; const s : string ); // v = vertical
 
 
@@ -63,9 +63,9 @@ interface uses xpc, crt, num, stri;
     in console mode, what we really want is width in characters on
     the screen, not the length of the actual string in memory. }
   //  maybe this should be called width?
-  function cLength( s : string ) : byte;         { length - color codes }
+  function cLength( s : string ) : integer;    { length - color codes }
   function cstrip( s : string ) : string;
-  function normaltext( s : string ) : string;
+  function normaltext( s : string; esc : char = trg ) : string;
   function cpadstr( s : string; len : byte;ch : char ) : string;
 
   { these highlight punctuation and box drawing
@@ -85,237 +85,207 @@ implementation
 
   { vertical colorxy }
   procedure Colorxyv( const x, y, c : byte;
-		      const s	   : string );
+		      const s : string );
     var i : byte;
   begin
     for i := 1 to length( s ) do
     begin
-      crt.TextAttr := c;
+      crt.TextColor( c );
       crt.GotoXy( x, y + i - 1 );
       write( s[ i ]);
     end;
   end;
 
-
   procedure colorxyc( x, y, c : byte; s : string );
   begin
-   colorxy( x + 1 - length( s ) div 2, y, c, s );
+    colorxy( x + 1 - length( s ) div 2, y, c, s );
   end;
 
- procedure cwcommand( cn : byte; s : string );
-   const digits = ['0','1','2','3','4','5','6','7','8','9'];
+  procedure cwcommand( cn : byte; s : string );
+    const digits = ['0','1','2','3','4','5','6','7','8','9'];
   begin
-   case cn of
-    cwchangefore:
-     if s[1] in ccolset
-      then tcolor := ( tcolor and $F0 ) + pos( s[1], ccolors ) - 1;
-    cwchangeback:
-     if s[1] in ccolset
-      then tcolor := (tcolor and $0F) + (pos( s[1], ccolors ) - 1) * 16;
-    cwCR :
-     begin
-      tXpos := 1;
-      typos := inc2( tYpos, 1, tYmax-tymin +2);
-      if
-       tYpos > tYmax - tymin + 1
-      then
-       begin
-        //  scrollup1( txmin, txmax, tymin, tymax, writeto );
-        tYpos := tYmax - tymin + 1;
-        txpos := 1;
-        cwrite('|%');
-       end;
-     end;
-    cwBS :
-     begin
-      if
-       tXpos <> 1
-      then
-       begin
-        colorxy( txmin+tXpos - 1, tymin+tYpos, tColor, ' ' );
-        dec( tXpos );
-       end;
-     end;
-    cwclrscr :
-     begin
-      //  fillbox( txmin, tymin, txmax, tymax, tcolor*256 + 32 );
-      txpos := 1;
-      typos := 1;
-     end;
-    cwclreol :
-      colorxy( txmin+txpos-1, tymin+typos-1,
-	      tcolor, stri.chntimes( ' ', txmax-txpos+1 ) );
-    cwsavecol :
-     sColor := tColor;
-    cwloadcol :
-     tColor := sColor;
-    cwchntimes :
-     begin
-      if length(s) <> 3 then exit;
-      if (s[2] in digits) and (s[3] in digits) then
-	cwrite( normaltext( stri.chntimes( s[1], s2n(s[2]+s[3])) ));
-     end;
-    cwgotoxy :
-     begin
-      if length( s ) <> 4 then exit;
-      if (s[1] in digits) and
-         (s[2] in digits) and
-         (s[3] in digits) and
-         (s[4] in digits)
-      then
-       begin
-        txpos := s2n( s[1]+s[2] );
-        typos := s2n( s[3]+s[4] );
-        gotoxy( txpos + txmin -1 , typos + tymin - 1 );
-       end;
-     end;
-    cwsavexy :
-     begin
-      txsav := txpos;
-      tysav := typos;
-     end;
-    cwloadxy :
-     begin
-      txpos := txsav;
-      typos := tysav;
-      gotoxy( txpos + txmin -1 , typos + tymin - 1 );
-   end;
-    cwspecialstr :
-      { //  if i want to do things like this i should make it a 'macro' callback
-     case upcase(s[1]) of
-      'P' : cwrite( thisdir );
-      'D' : cwrite( stardate );
-     end;
-	}
-      ;
-    cwrenegade : tcolor := s2n( s );
-   end; { of case cn }
+    case cn of
+      cwchangefore : if s[ 1 ] in ccolset then
+		       tcolor := ( tcolor and $F0 ) + pos( s[ 1 ], ccolors ) - 1;
+      cwchangeback : if s[ 1 ] in ccolset then
+		       tcolor := ( tcolor and $0F ) + ( pos( s[ 1 ], ccolors ) - 1 ) * 16;
+      cwCR	   : begin
+		       tXpos := 1;
+		       typos := inc2( tYpos, 1, tYmax - tymin + 2 );
+		       if tYpos > tYmax - tymin + 1 then
+		       begin
+			 //  scrollup1( txmin, txmax, tymin, tymax, writeto );
+			 tYpos := tYmax - tymin + 1;
+			 txpos := 1;
+			 cwrite( trg + '%' );
+		       end;
+		     end;
+      cwBS	   : if tXpos <> 1 then
+		       begin
+			 colorxy( txmin + tXpos - 1, tymin + tYpos, tColor, ' ' );
+			 dec( tXpos );
+		       end;
+      cwclrscr	   : begin
+		       //  fillbox( txmin, tymin, txmax, tymax, tcolor*256 + 32 );
+		       txpos := 1;
+		       typos := 1;
+		     end;
+      cwclreol	   : colorxy( txmin + txpos - 1, tymin + typos - 1,
+			     tcolor, stri.chntimes( ' ', txmax - txpos + 1 ) );
+      cwsavecol	   : sColor := tColor;
+      cwloadcol	   : tColor := sColor;
+      cwchntimes   : begin
+		       if length(s) <> 3 then exit;
+		       if ( s[ 2 ] in digits ) and ( s[ 3 ] in digits ) then
+			 cwrite( normaltext( stri.chntimes( s[ 1 ], s2n( s[ 2 ] + s[ 3 ])) ));
+		     end;
+      cwgotoxy	   : begin
+		       if length( s ) <> 4 then exit;
+		       if ( s[ 1 ] in digits )
+			 and ( s[ 2 ] in digits )
+			 and ( s[ 3 ] in digits )
+			 and ( s[ 4 ] in digits )
+			 then
+		       begin
+			 txpos := s2n( s[ 1 ] + s[ 2 ]);
+			 typos := s2n( s[ 3 ] + s[ 4 ]);
+			 gotoxy( txpos + txmin - 1 , typos + tymin - 1 );
+		       end;
+		     end;
+      cwsavexy	   : begin
+		       txsav := txpos;
+		       tysav := typos;
+		     end;
+      cwloadxy	   : begin
+		       txpos := txsav;
+		       typos := tysav;
+		       gotoxy( txpos + txmin -1 , typos + tymin - 1 );
+		     end;
+      cwspecialstr :
+	{ //  if i want to do things like this i should make it a 'macro' callback
+	  case upcase(s[1]) of
+	  'P' : cwrite( thisdir );
+	  'D' : cwrite( stardate );
+	  end; } ;
+	cwrenegade : tcolor := s2n( s );
+    end; { of case cn }
   end; { of cwcommand }
 
- procedure cwrite( s : string );
-  var
-   b : byte;
-  procedure write( s : string );
-   begin
-    colorxy( tXmin+tXpos-1, tYmin+tYpos-1, tColor, s );
-    inc( tXpos );
-    if
-     tXpos > tXmax-txmin+1
-    then
-     cwrite( #13 );
-   end;
+  procedure cwrite( s : string );
+    var b : byte;
+    procedure write( s : string );
+    begin
+      colorxy( tXmin + tXpos - 1, tYmin + tYpos - 1, tColor, s );
+      inc( tXpos );
+      if tXpos > tXmax - txmin + 1 then cwrite( #13 );
+    end;
   begin
-   if s = '' then exit; {0311.95: i never bothered to check that!!}
-   b := 0;
-   repeat
-    inc( b );
-    if
-     not cwcommandmode
-    then
-     case s[b] of
-      '|' : cwcommandmode := true;
-      #13 : cwcommand( cwcr, '' );
-      #08 : cwcommand( cwbs, '' );
-      else write( s[b] );
-     end
-    else
-     case cwcurrenttask of
-      cwnotask : begin
-                  case s[b] of
-                   '|': write('|');
-                   '_': begin
-                         cwcommandmode := false;
-                         cwrite(#13);
-                        end;
-                   '!': cwcurrenttask := cwchangeback;
-                   '@': begin
-                         cwcurrenttask := cwgotoxy;
-                         cwnchexpected := 4;
-                        end;
-                   '#': begin
-                         cwcurrenttask := cwchntimes;
-                         cwnchexpected := 3;
-                        end;
-                   '$': begin
-                         cwcommandmode := false;
-                         cwcommand( cwclrscr, '' );
-                        end;
-                   '%': begin
-                         cwcommandmode := false;
-                         cwcurrenttask := cwnotask;
-                         cwcommand( cwclreol, '' );
-                        end;
-                   '^': begin
-                         cwcurrenttask := cwspecialstr;
-                         cwnchexpected := 1;
-                        end;
-                   ')': cwcommand( cwsavecol, '' );
-                   '(': cwcommand( cwloadcol, '' );
-                   ']': cwcommand( cwsavexy, '' );
-                   '[': cwcommand( cwloadxy, '' );
-                   '0'..'9': begin
-                               cwdigit1 := s[b];
-                               cwcurrenttask := cwrenegade;
-                               cwnchexpected := 1;
-                             end;
-                   else
-                    if s[b] in ccolset then cwcommand( cwchangefore, s[b] );
-                  end;
-                  if (cwcurrenttask = cwnotask) then cwcommandmode := false;
-                 end;
-      cwchangeback : begin
-                      if
-                       s[b] in ccolset
-                      then
-                       cwcommand( cwchangeback, s[b] );
-                      cwcurrenttask := cwnotask;
-                      cwcommandmode := false;
-                     end;
-      cwgotoxy : begin
-                  case cwnchexpected of
-                   4 : cwdigit1 := s[b];
-                   3 : cwdigit2 := s[b];
-                   2 : cwdigit3 := s[b];
-                   1 : begin
-                        cwcommandmode := false;
-                        cwcurrenttask := cwnotask;
-                        cwcommand( cwgotoxy,
-                                   cwdigit1+cwdigit2+cwdigit3+s[b] );
-                       end;
-                  end;
-                  dec( cwnchexpected );
-                 end;
-      cwchntimes : begin
-                    case cwnchexpected of
-                     3 : cwchar := s[b];
-                     2 : cwdigit1 := s[b];
-                     1 : begin
-                          cwcommandmode := false;
-                          cwcurrenttask := cwnotask;
-                          cwcommand( cwchntimes, cwchar+cwdigit1+s[b] );
-                         end;
-                    end;
-                    dec( cwnchexpected );
-                   end;
-      cwspecialstr : if cwnchexpected = 1 then
-                     begin
-                      cwcommandmode := false;
-                      cwcurrenttask := cwnotask;
-                      dec( cwnchexpected );
-                      cwcommand( cwspecialstr, s[b] );
-                     end;
-      cwrenegade : if cwnchexpected = 1 then
-                    begin
-                     cwcommandmode := false;
-                     cwcurrenttask := cwnotask;
-                     dec( cwnchexpected );
-                     cwcommand( cwrenegade, cwdigit1 + s[b] );
-                    end;
-     end;
-   until b = length(s);
-  gotoxy( txmin+txpos-1, tymin+typos-1 );
-  textattr := tcolor;
- end;
+    if s = '' then exit; {0311.95: i never bothered to check that!!}
+    b := 0;
+    repeat
+      inc( b );
+      if not cwcommandmode then
+	case s[ b ] of
+	  trg : cwcommandmode := true;
+	  #13 : cwcommand( cwcr, '' );
+	  #08 : cwcommand( cwbs, '' );
+	  else write( s[ b ]);
+	end
+      else
+	case cwcurrenttask of
+	  cwnotask     : begin
+		       case s[ b ] of
+			 '|' : write( '|' );
+			 '_' : begin
+				cwcommandmode := false;
+				cwrite(#13);
+			      end;
+			 '!' : cwcurrenttask := cwchangeback;
+			 '@' : begin
+				cwcurrenttask := cwgotoxy;
+				cwnchexpected := 4;
+			      end;
+			 '#' : begin
+				cwcurrenttask := cwchntimes;
+				cwnchexpected := 3;
+			      end;
+			 '$' : begin
+				cwcommandmode := false;
+				cwcommand( cwclrscr, '' );
+			      end;
+			 '%' : begin
+				cwcommandmode := false;
+				cwcurrenttask := cwnotask;
+				cwcommand( cwclreol, '' );
+			      end;
+			 '^' : begin
+				cwcurrenttask := cwspecialstr;
+				cwnchexpected := 1;
+			      end;
+			 ')' : cwcommand( cwsavecol, '' );
+			 '(' : cwcommand( cwloadcol, '' );
+			 ']' : cwcommand( cwsavexy, '' );
+			 '[' : cwcommand( cwloadxy, '' );
+			 '0'..'9': begin
+				     cwdigit1 := s[ b ];
+				     cwcurrenttask := cwrenegade;
+				     cwnchexpected := 1;
+				   end;
+			 else
+			   if s[ b ] in ccolset then cwcommand( cwchangefore, s[ b ] );
+		       end;
+		       if ( cwcurrenttask = cwnotask ) then cwcommandmode := false;
+		     end;
+	  cwchangeback : begin
+			   if s[b] in ccolset then cwcommand( cwchangeback, s[ b ] );
+			   cwcurrenttask := cwnotask;
+			   cwcommandmode := false;
+			 end;
+	  cwgotoxy     : begin
+		       case cwnchexpected of
+			 4 : cwdigit1 := s[ b ];
+			 3 : cwdigit2 := s[ b ];
+			 2 : cwdigit3 := s[ b ];
+			 1 : begin
+			       cwcommandmode := false;
+			       cwcurrenttask := cwnotask;
+			       cwcommand( cwgotoxy,
+					 cwdigit1 + cwdigit2 + cwdigit3 + s[ b ]);
+			     end;
+		       end;
+		       dec( cwnchexpected );
+		     end;
+	  cwchntimes   : begin
+			 case cwnchexpected of
+			   3 : cwchar := s[ b ];
+			   2 : cwdigit1 := s[ b ];
+			   1 : begin
+				 cwcommandmode := false;
+				 cwcurrenttask := cwnotask;
+				 cwcommand( cwchntimes, cwchar + cwdigit1 + s[ b ]);
+			       end;
+			 end;
+			 dec( cwnchexpected );
+		       end;
+	  cwspecialstr : if cwnchexpected = 1 then
+			 begin
+			   cwcommandmode := false;
+			   cwcurrenttask := cwnotask;
+			   dec( cwnchexpected );
+			   cwcommand( cwspecialstr, s[ b ]);
+			 end;
+	  cwrenegade   : if cwnchexpected = 1 then
+		       begin
+			 cwcommandmode := false;
+			 cwcurrenttask := cwnotask;
+			 dec( cwnchexpected );
+			 cwcommand( cwrenegade, cwdigit1 + s[ b ]);
+		       end;
+	end;
+    until b = length( s );
+    gotoxy( txmin + txpos - 1, tymin + typos - 1 );
+    textattr := tcolor;
+  end;
 
   procedure cwriteln( s : string );
   begin cwrite( s ); write( #13 ); writeln;
@@ -326,132 +296,111 @@ implementation
   begin
     for i := 0 to high( args ) do
       case args[ i ].vtype of
-	vtinteger    : cwrite( n2s( args[ i ].vinteger ));
-	vtstring     : cwrite( args[ i ].vstring^ );
+	vtinteger : cwrite( n2s( args[ i ].vinteger ));
+	vtstring  : cwrite( args[ i ].vstring^ );
 	vtansistring : cwrite( ansistring( args[ i ].vansistring ));
       end;
-     write( #13, #10 ); writeln;
+    write( #13, #10 ); writeln;
   end;
 
- procedure cwritexy( x, y : byte; s : string );
-   var c, d : byte;
+  procedure cwritexy( x, y : byte; s : string );
+    var c, d : byte;
   begin
-   c := txpos;
-   d := typos;
-   txpos := x;
-   typos := y;
-   cwrite( s );
-   txpos := c;
-   typos := d;
+    gotoxy( x, y );
+    cwrite( s );
   end;
 
- procedure ccenterxy( x, y : byte; s : string );
+  procedure ccenterxy( x, y : byte; s : string );
   begin
-   cwritexy( x + 1 - clength( s ) div 2, y, s );
+    cwritexy( x + 1 - clength( s ) div 2, y, s );
   end;
 
- Procedure StWrite(S: string);
-  var
-    counter : byte;
+  procedure StWrite( s: string );
+    var counter : byte;
   begin
     for counter := 1 to Length(S) do
-     begin
-       case S[counter] of
-        'a'..'z','0'..'9','A'..'Z',' ' : TColor := $0F;
-        '[',']','(',')','{','}','<','>','"' : TColor := $09;
-        //  '░'..'▀' : TColor := $08;
-        else TColor := $07;
-       end;
-       cwrite(s[counter]);
-     end;
+    begin
+      case S[counter] of
+	'a'..'z','0'..'9','A'..'Z',' ' : TColor := $0F;
+	'[',']','(',')','{','}','<','>','"' : TColor := $09;
+	//  '░'..'▀' : TColor := $08;
+	else TColor := $07;
+      end;
+      cwrite(s[counter]);
+    end;
   end;
 
- procedure StWriteln( s : string );
+  procedure StWriteln( s : string );
   begin
-   stwrite( s + #13 );
+    stwrite( s + #13 );
   end;
 
- procedure StWritexy( x, y : byte; s : string );
+  procedure StWritexy( x, y : byte; s : string );
   begin
-   txpos := x;
-   typos := y;
-   stwrite( s );
+    txpos := x;
+    typos := y;
+    stwrite( s );
   end;
-
-
 
 { ■ string formatting commands }
 
- function clength( s : string ) : byte;
-  var
-   i,c : byte;
+  function clength( s : string ) : integer;
+    var i, c : integer;
   begin
-   c := 0;
-   i := 1;
-   while i <= length(s) do
-    if
-     s[ i ] = '|'
-    then
-     case s[i+1] of
-      '@' : inc( i, 5 );
-      '#' : inc( i, 4 );
-      else inc( i, 2 );
-     end
-    else
-     begin
-      inc( c );
-      inc( i );
-     end;
-   clength := c;
+    c := 0;
+    i := 1;
+    while i <= length( s ) do
+      if ( s[ i ] = trg ) and ( i + 1 <= length( s )) then
+	case s[ i + 1 ] of
+	  '@' : inc( i, 5 );
+	  '#' : inc( i, 4 );
+	  else inc( i, 2 );
+	end
+      else
+      begin
+	inc( c );
+	inc( i );
+      end;
+    clength := c;
   end;
 
- function cstrip( s : string ) : string;
-  var
-   i : byte;
-   c : string;
+  function cstrip( s : string ) : string;
+    var i : integer;
   begin
-   c := '';
-   i := 1;
-   while i <= length(s) do
-    if
-     s[ i ] = '|'
-    then
-     case s[i+1] of
-      '@' : inc( i, 5 );
-      '#' : inc( i, 4 );
-      else inc( i, 2 );
-     end
-    else
-     begin
-      c := c + s[i];
-      inc( i );
-     end;
-   cstrip := c;
+    result := '';
+    i := 1;
+    while i <= length( s ) do
+      if s[ i ] = trg then
+	case s[ i + 1 ] of
+	  '@' : inc( i, 5 );
+	  '#' : inc( i, 4 );
+	  else inc( i, 2 );
+	end
+      else begin
+	result := result + s[ i ];
+	inc( i );
+      end
   end;
 
 
- function normaltext( s : string ) : string;
-   var
-    c : byte;
-    t : string;
-   begin
-    t := '';
-    for c := 1 to length( s ) do
-     if
-      s[c] = '|'
-     then
-      t := t + '|' + s[c]
-     else
-      t := t + s[c];
-     normaltext := t;
-   end;
+  {  probably want to rename this to 'escape' }
+  function normaltext( s : string; esc : char = trg ) : string;
+    var i : integer;
+  begin
+    result := '';
+    for i := 1 to length( s ) do
+    begin
+      if s[ i ] = trg then result := result + esc;
+      result := result + s[ i ];
+    end;
+  end;
 
 
- function cpadstr( s : string; len : byte; ch : char ) : string;
+  function cpadstr( s : string; len : byte; ch : char ) : string;
   begin
     if clength( s ) > len then s := stri.trunc( s, len );
-   while clength( s ) < len do s := s + ch;
-   cpadstr := s;
+    while clength( s ) < len do s := s + ch;
+    cpadstr := s;
   end;
 
 
