f232b907b0af5495fe45210ae0e4d07d02f85366
tangentstorm <michal.wallace@gmail.com>
2012-12-29 18:43:18 -0600

    implemented a generic cursor for the generic lists

--- code/ll.pas
+++ code/ll.pas
@@ -4,32 +4,46 @@ interface uses xpc, sysutils;
 
 type
   generic list<T> = class
-  private
-    type
-      link       = class
-        next, prev : link;
-        value	 : T;
+    private type link = class
+      next, prev : link;
+      value	 : T;
       public
-        constructor create( val : T );
-      end;
-      tlist      = specialize list<t>;
-      iter       = class
+      constructor create( val : T );
+    end;
+    private type specialized  = specialize list<t>;
+    public type cursor = class
       private
-        _lis : tlist;
+        _lis : specialized;
         _cur : link;
-        function _value : T;
+        _idx : cardinal;
+        function _get_value : T;
+        function _get_index : cardinal;
       public
-        constructor create( lis : tlist );
+        constructor create( lis : specialized );
+        procedure reset;
+        procedure to_top;
+        procedure to_end;
+        function move_next : boolean;
+        function move_prev : boolean;
+        function next( out t : T ) : boolean;
+        function prev( out t : T ) : boolean;
+        property value : T
+          read _get_value;
+        property index : cardinal
+          read _get_index;
+      public  { for..in loop interface }
         function movenext : boolean;
-        property current : T read _value;
-      end;
-      listaction = procedure( var n : T );
-      predicate  = function( n : T ) : Boolean;
-  protected
-    _last, _head : link;
+        property current  : t
+          read _get_value;
+    end;
+    type listaction = procedure( var n : T );
+    type predicate  = function( n : T ) : Boolean;
+
+   protected
+    _first_link, _last_link : link;
     count : integer;
-    function addFirstOne( ln : link ) : boolean;
-  public
+    function is_first_insert( ln : link ) : boolean;
+   public
     constructor create;
     procedure append( val : T );
     procedure insert( val : T );
@@ -40,9 +54,10 @@ type
     function is_empty: boolean;
     function first : T;
     function last : T;
-    function getenumerator : iter;
-
-    { -- old interface --}
+    function make_cursor : list.cursor;
+   public { -- interface for for..in loops --}
+    function getenumerator : list.cursor;
+   public { -- ancient deprecated interface --}
     function next( const n : T ) : T; deprecated;
     function prev( const n : T ) : T; deprecated;
     function empty: boolean; deprecated;
@@ -61,30 +76,91 @@ implementation
     self.value := val;
   end;
 
-  { -- list iterator ( internal type ) -- }
+  { -- list cursor ( internal type ) -- }
 
-  constructor list.iter.create( lis : tlist );
+  constructor list.cursor.create( lis : specialized );
   begin
     _lis := lis;
+    self.reset;
+  end;
+
+  procedure list.cursor.reset;
+  begin
     _cur := nil;
+    _idx := 0;
   end;
 
-  function list.iter.movenext : boolean;
+  function list.cursor.move_next : boolean;
   begin
-    if _cur = nil then _cur := _lis._head
-    else _cur := _cur.next;
-    result := ( _cur <> nil );
+    if _cur = nil then begin
+      _cur := _lis._first_link;
+      _idx := 1;
+    end
+    else begin
+      _cur := _cur.next;
+      inc( _idx );
+    end;
+    result := ( _cur <> nil )
   end;
 
-  function list.iter._value : t;
+  { this is only here to allow for..in loops }
+  function list.cursor.movenext : boolean; inline;
+  begin result := self.move_next end;
+
+  function list.cursor.move_prev : boolean;
   begin
-    result := _cur.value
+    if _cur = nil then result := false
+    else begin
+      _cur := _cur.prev;
+      dec( _idx );
+      result := ( _cur <> nil );
+    end
+  end;
+
+  procedure list.cursor.to_top;
+  begin
+    self.reset;
+    if not self.move_next then
+      raise Exception.create('no top item to go to');
+  end;
+
+  procedure list.cursor.to_end;
+  begin
+    _cur := _lis._last_link;
+  end;
+
+  function list.cursor.next( out t : t ) : boolean;
+  begin
+    result := self.move_next;
+    if result then t := _cur.value;
+  end;
+
+  function list.cursor.prev( out t : t ) : boolean;
+  begin
+    result := self.move_prev;
+    if result then t := _cur.value;
+  end;
+
+  function list.cursor._get_value : t;
+  begin
+    if _cur = nil then raise Exception.create( 'the list is empty' )
+    else result := _cur.value
+  end;
+
+  function list.cursor._get_index : cardinal;
+  begin
+    result := _idx;
+  end;
+
+  function list.make_cursor : cursor;
+  begin
+    result := cursor.create( self )
   end;
 
   { this allows 'for .. in' in the fpc / delphi compilers }
-  function list.getenumerator: iter;
+  function list.getenumerator: cursor;
   begin
-    result := iter.create( self );
+    result := self.make_cursor
   end;
 
 
@@ -92,19 +168,17 @@ implementation
 
   constructor list.create;
   begin
-    _head := nil; _last := nil; count := 0;
+    _first_link := nil; _last_link := nil; count := 0;
   end;
 
-  function List.find( pred : Predicate ) : T;
-    var it : iter; found : boolean = false;
+  function List.find( pred : Predicate ) : t;
+    var cur : cursor; found : boolean = false;
   begin
-    it := iter.create( self );
+    cur := cursor.create( self );
     repeat
-      found := pred( it.current )
-    until found or not it.movenext;
-    if found
-      then result := it.current
-      { else result := nil }
+      found := pred( cur.value )
+    until found or not cur.move_next;
+    if found then result := cur.value
   end; { find }
 
   procedure list.foreachdo( what : listaction ); inline; deprecated;
@@ -115,7 +189,7 @@ implementation
   procedure list.foreach( what : listaction );
     var p, q : link;
   begin
-    p := self._head;
+    p := self._first_link;
     while p <> nil do
     begin
       q := p;
@@ -127,12 +201,12 @@ implementation
 
 
   { helper routine for insert / append }
-  function list.addFirstOne( ln : link ) : boolean;
+  function list.is_first_insert( ln : link ) : boolean;
   begin
     inc( self.count );
-    if self._head = nil then begin
-      self._last := ln;
-      self._head := ln;
+    if _first_link = nil then begin
+      _last_link := ln;
+      _first_link := ln;
       result := true;
     end
     else result := false;
@@ -144,11 +218,11 @@ implementation
     var ln : link;
   begin
     ln := link.create( val );
-    if not addFirstOne( ln ) then
+    if not is_first_insert( ln ) then
     begin
-      ln.next := _head;
-      _head.prev := ln;
-      _head := ln;
+      ln.next := _first_link;
+      _first_link.prev := ln;
+      _first_link := ln;
     end;
   end; { insert }
 
@@ -157,11 +231,11 @@ implementation
     var ln : link;
   begin
     ln := link.create( val );
-    if not addFirstOne( ln ) then
+    if not is_first_insert( ln ) then
     begin
-      ln.prev := _last;
-      _last.next := ln;
-      _last := ln;
+      ln.prev := _last_link;
+      _last_link.next := ln;
+      _last_link := ln;
     end;
   end; { append }
 
@@ -170,39 +244,39 @@ implementation
     var p : link;
   begin
     if not self.is_empty then begin
-      p := self._head;
+      p := _first_link;
       while ( p.next.value <> val )
-	and ( p.next <> _last ) do
+        and ( p.next <> _last_link ) do
 	p := p.next;
 
       if p.next.value = val then begin
 	p.next := p.next.next;
 	if self.last = val then
-	  if p.value = val then _last := nil
-	  else _last := p
+          if p.value = val then _last_link := nil
+          else _last_link := p
       end
     end
   end; { remove }
 
   procedure list.drop;
   begin
-    self._last := self._last.prev;
-    self._last.next := nil;
+    self._last_link := self._last_link.prev;
+    self._last_link.next := nil;
   end;
 
   function list.is_empty : boolean;
-  begin result := _last = nil;
+  begin result := _last_link = nil;
   end;
 
   function list.first : t;
   begin
-    if _head <> nil then result := _head.value
+    if _first_link <> nil then result := _first_link.value
     else raise Exception.create('empty list has no first member.');
   end; { first }
 
   function list.last: T;
   begin
-    if assigned( _last ) then result := _last.value
+    if assigned( _last_link ) then result := _last_link.value
     else raise Exception.create('empty list has no last member.');
   end; { last }
 
