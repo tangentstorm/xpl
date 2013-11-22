ed842d7aaf0b567f72b1390ece84a19bd5b24fd8
tangentstorm <michal.wallace@gmail.com>
2013-11-22 06:50:35 -0600

    sd: droped old gpc implementation merged sd_fpc.inc

--- code/sd.pas
+++ code/sd.pas
@@ -2,7 +2,7 @@
 | storage device
 |
 | -------------------------------------------------
-| copyright (c) 2012 michal j. wallace
+| copyright (c) 2012,2013 michal j. wallace
 | see LICENSE.org for usage information
 }
 {$mode objfpc}{$i xpc.inc}
@@ -12,7 +12,7 @@ interface uses log, fs, sysutils;
 
 type
   TBlock = array [ 0..1023 ] of byte;
-  TDrive = object {  object because of gpc... probably should drop }
+  TDrive = class
     constructor init ( path : string );
     procedure wipe;
     procedure grow ( n : byte );
@@ -29,13 +29,53 @@ var
   main : TDrive;
 
 implementation
-  var
-    empty_block	: TBlock;
-  {$IFDEF GPC}
-    {$i sd_gpc.inc}
-  {$ELSE}
-    {$i sd_fpc.inc}
-  {$ENDIF}
+var
+  empty_block : TBlock;
+
+constructor TDrive.init ( path : string );
+  begin
+    // change to fs.open when it's ready
+    fs.update( mFile, path );
+  end;
+
+procedure TDrive.wipe;
+  begin
+    seek( mFile, 0 );
+    truncate( mFile );
+  end;
+
+procedure TDrive.grow ( n : byte );
+  begin
+    seek( mFile, filesize( mfile ));
+    for n := n downto 1 do write( mFile, empty_block );
+  end;
+
+procedure TDrive.load ( i : cardinal; var b : TBlock );
+  begin
+    seek( mFile, i );
+    read( mFile, b );
+  end;
+
+procedure TDrive.save ( i : cardinal; var b : TBlock );
+  begin
+    seek( mFile, i );
+    write( mFile, b );
+  end;
+
+function TDrive.block_count : cardinal;
+  begin
+    result := filesize( mFile )
+  end;
+
+function TDrive.byte_count : cardinal;
+  begin
+    result := self.block_count * sizeOf( TBlock );
+  end;
+
+destructor TDrive.done;
+  begin
+    close( mfile );
+  end;
 
 begin
   FillDWord( empty_block, 256, 0 );
