dca79d80cc7703f2c93af9b66ad0deca2548bd3d
tangentstorm <michal.wallace@gmail.com>
2014-04-11 22:55:44 -0500

    test_kvm: added a test for hookterm

--- test/test_kvm.pas
+++ test/test_kvm.pas
@@ -1,4 +1,29 @@
 {$mode delphiunicode}{$i xpc.inc}{$i test_kvm.def}
 implementation uses kvm;
 
+procedure test_hookterm;
+  var base : kvm.TBaseTerm; hook : kvm.THookTerm; old : word;
+  begin
+    old := kvm.textattr; kvm.textattr := $abcd;
+    try
+      hook := THookTerm.Create;
+      hook.textattr := $1234;
+      chk.equal($1234, kvm.textattr);
+      chk.equal($1234, hook.asterm.textattr);
+      chk.equal($1234, hook.textattr);
+
+      base := TBaseTerm.Create(10,10);
+      base.textattr := $9999;
+      chk.equal($9999, base.textattr);
+
+      hook.subject := base;
+      hook.textattr := $1234;
+      chk.equal($1234, base.textattr);
+
+      hook.asTerm.textattr := $2345;
+      chk.equal($2345, base.textattr);
+
+    finally kvm.textattr := old end;
+  end;
+
 end.
