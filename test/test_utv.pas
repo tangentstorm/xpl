946ea6b7c009ca3c373aa82b012bbf78de947c2f
tangentstorm <michal.wallace@gmail.com>
2014-04-11 22:56:21 -0500

    test_utv: added some tests for pointer issues

--- test/test_utv.pas
+++ test/test_utv.pas
@@ -1,4 +1,19 @@
 {$mode delphiunicode}{$i xpc.inc}{$i test_utv.def}
-implementation uses utv;
+implementation uses utv, kvm;
+
+
+procedure test_view;
+  var tv : utv.TView;
+  begin tv := tview.create(nil); tv.free;
+  end;
+
+procedure test_termview_pointers;
+  var tv : utv.TTermView; hook : THookTerm;
+  begin
+    tv := TTermView.Create(Nil);
+    chk.equal(tv.asterm.textattr, $0807,   'this works');
+    chk.equal(tv.asterm.textattr, $0807,   'hopefully this won''t fail');
+    tv.free; // and hopefully this won't fail either!
+  end;
 
 end.
