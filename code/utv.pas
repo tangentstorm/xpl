9864523d735ab2f1c0dbcfa0c90702e6bf934334
tangentstorm <michal.wallace@gmail.com>
2014-04-11 23:03:33 -0500

    utv: rebased TView on TBounds2D, added visibility

--- code/utv.pas
+++ code/utv.pas
@@ -2,53 +2,49 @@
 //
 // Copyright © 2014 Michal J Wallace http://tangentstorm.com/
 // Available for use under the MIT license. See LICENSE.txt
-//
+
 {$i xpc.inc}{$mode delphi}
 unit utv;
 interface uses xpc, classes, kvm, arrays, cli, ug2d, num, cw, math, ustr, chk;
-
 type
-  TView = class(TComponent, IPoint2D, ISize2D, IBounds2D)
+  TView = class(ug2d.TBounds2D)
     protected
-      _x, _y : integer;
-      _w, _h : cardinal;
       _bg, _fg : byte;
-      _dirty : boolean;
+      _dirty : boolean; _visible : boolean;
       _ioctx : ITerm; // kvm parent term
       _views : GArray<TView>; // subviews
-      function GetX : integer;  procedure SetX(value : integer);
-      function GetY : integer;  procedure SetY(value : integer);
-      function GetW : cardinal; procedure SetW(value : cardinal);
-      function GetH : cardinal; procedure SetH(value : cardinal);
     public
       constructor Create( aOwner : TComponent ); override;
+      function Init( x, y : integer ; w, h : cardinal ) : TView; reintroduce;
       destructor Destroy; override;
       procedure Update; virtual;
       procedure Smudge;
+      procedure SetVisible( val : boolean ); virtual;
+      procedure Show; procedure Hide;
       property dirty : boolean read _dirty write _dirty;
     published
-      property x : integer read _x write SetX;
-      property y : integer read _y write SetY;
-      property w : cardinal read _w write SetW;
-      property h : cardinal read _h write SetH;
-      procedure Nudge(dx, dy : integer);
+      property visible : boolean read _visible write _visible;
       procedure Render; virtual;
-      procedure Resize(new_w, new_h : cardinal); virtual;
+      procedure Resize(new_w, new_h : cardinal); override;
+      procedure dump; virtual;
     end;
 
   // A class with its own video ram buffer:
   TTermView = class (TView, ITerm)
-     protected
-      _asIterm  : ITerm;
+    protected
       _hookterm : THookTerm;
       _gridterm : TGridTerm;
       procedure _OnGridChange( msg : TTermMessage; args : array of variant );
-   public
+    public
       constructor Create( aOwner : TComponent ); override;
-      property term : ITerm read _asITerm implements ITerm;
+      function Init( x, y : integer ; w, h : cardinal ) : TTermView; reintroduce;
+      destructor Destroy; override;
+      function asTerm : ITerm; // weak reference
+      property term : ITerm read asTerm implements ITerm;
     published
       procedure Render; override;
       procedure Resize(new_w, new_h : cardinal); override;
+      procedure dump; override;
     end;
 
   TStepper = class (TComponent)
@@ -70,6 +66,11 @@ constructor TView.Create( aOwner : TComponent );
     _x := 0; _y := 0; _w := 30; _h := 10;
     _bg := $FC; _fg := $0; _dirty := true;
     _views := GArray<TView>.Create();
+    _visible := true;
+  end;
+
+function TView.Init( x, y : integer ; w, h : cardinal ) : TView;
+  begin inherited; result := self;
   end;
 
 destructor TView.Destroy;
@@ -77,53 +78,69 @@ destructor TView.Destroy;
     _views.Free; // members are TViews, so owners will free
     inherited Destroy;
   end;
-
-procedure TView.SetX(value : integer); begin _x := value; end;
-procedure TView.SetY(value : integer); begin _y := value; end;
-procedure TView.SetW(value : cardinal); begin resize(value, _h) end;
-procedure TView.SetH(value : cardinal); begin resize(_w, value) end;
-
-function TView.GetX : integer; begin result := _x end;
-function TView.GetY : integer; begin result := _y end;
-function TView.GetW : cardinal; begin result := _w end;
-function TView.GetH : cardinal; begin result := _h end;
-
-procedure TView.Nudge(dx, dy : integer);
-  begin
-    _x += dx; _y += dy;
-  end;
 
-
-type
-  Weak<T:IUnknown> = class
-    class function Ref(obj : T) : T;
-    end;
-
-class function Weak<T>.Ref(obj : T) : T;
-  begin obj._addRef; result := obj;
-  end;
 
 procedure TView.Update;
   var child : TView;
   begin
-    _ioctx := Weak<ITerm>.Ref(kvm.work);
-    kvm.SubTerm(_x, _y, _w, _h);
-    try kvm.bg(_bg); kvm.fg(_fg);
-      if _dirty then begin Render; _dirty := false; end;
-      for child in _views do child.Update;
-    finally kvm.PopTerm; end;
+    if _visible then begin
+      _ioctx := kvm.asTerm;
+      kvm.SubTerm(_x, _y, _w, _h);
+      try kvm.bg(_bg); kvm.fg(_fg);
+        if _dirty then begin Render; _dirty := false; end;
+        for child in _views do child.Update;
+      finally kvm.PopTerm end
+    end;
   end;
+
+
+procedure tview.dump;
+  begin
+    trace(['TView', self.classname]);
+    indent; begin
+      trace(['x:', x, ' ', 'y:', y]);
+      trace(['w:', w, ' ', 'h:', h]);
+    end; dedent;
+  end; { tview.dump }
+
+procedure ttermview.dump;
+begin
+  assert( self <> nil );
+  trace('--[ utv.pas::ttermview.dump ]-------');
+  trace(['TERMVIEW[', self.classname, ']']);
+  indent;
+  { }trace(['_gridterm:']); indent; _gridterm.dump; dedent;
+  { }trace(['_hookterm:']); indent; _hookterm.dump; dedent;
+  dedent;
+  trace('--[ done with termview.dump ]-------');
+end;
+
+
+  
+{ display management }
 
 procedure TView.Smudge;
   begin _dirty := true;
   end;
-  
+
+procedure TView.SetVisible( val : boolean );
+  begin _visible := val; smudge;
+  end;
+
+procedure TView.Show;
+  begin SetVisible( true );
+  end;
+
+procedure TView.Hide;
+  begin SetVisible( false )
+  end;
+
 procedure TView.Render;
   begin ClrScr
   end;
 
 procedure TView.Resize(new_w, new_h : cardinal);
-  begin _w := new_w; _h := new_h; Smudge;
+  begin inherited; Smudge;
   end;
 
 constructor TTermView.Create( aOwner : TComponent );
@@ -134,11 +151,27 @@ constructor TTermView.Create( aOwner : TComponent );
     _gridterm.fg(7);
     resize(32, 16);
     _hookterm := THookTerm.Create;
-    _hookterm.subject := _gridterm;
+    _hookterm.subject := _gridterm.asTerm;
     _hookterm.OnChange := _OnGridChange;
-    _asITerm := _hookterm;
   end;
 
+function TTermView.Init( x, y : integer ; w, h : cardinal ) : TTermView;
+  begin inherited; result := self
+  end;
+
+function TTermView.asTerm : ITerm;
+  begin result := _hookterm.asTerm;
+  end;
+
+destructor TTermView.Destroy;
+  begin
+    //   figure out why this gives invalid pointer operation
+    // if assigned(_gridterm) then _gridterm.free;
+    // if assigned(_hookterm) then _hookterm.free;
+    inherited;
+  end;
+
+
 procedure TTermView._OnGridChange( msg : TTermMessage; args : array of variant );
   begin self.smudge;
   end;
