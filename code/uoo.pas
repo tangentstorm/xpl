4d14177445e43471fc125daf80d43f83ad1dbb5b
tangentstorm <michal.wallace@gmail.com>
2014-04-16 23:12:30 -0500

    set umsg, uoo as object hierarchy root.

--- code/uoo.pas
+++ code/uoo.pas
@@ -6,12 +6,25 @@
 unit uoo;
 interface uses xpc, classes;
 type
+  tTypId = int32;
+
   tBuf = class (TComponent)
     {
     | Base class for typed data.
     }
-  end;
+    public
+      typ : tTypId;
+    end;
 
+  {
+  | there are four basic signatures, but for message
+  | passing we will mostly deal with with (Na,Ti)
+  }
+  tVoMethod = procedure of object;
+  tRuMethod = procedure ( buf : tBuf ) of object;
+  tNaMethod = function : tBuf of object;
+  tTiMethod = function ( buf : tBuf ) : tBuf of object;
+
   tObj = class (tBuf)
     {
     | Base class for objects.
@@ -19,17 +32,26 @@ type
     | Fundamentally, an object is a stateful machine
     | that sends and receives messages.
     }
-    function  eval( buf : tBuf ) : tBuf;
-    procedure exec( buf : tBuf );
+    protected
+      _OnNext : tNaMethod;
+      _OnSend : tTiMethod;
+    published
+      function  eval( buf : tBuf ) : tBuf; virtual;
+      procedure exec( buf : tBuf ); virtual;
+      property OnNext : tNaMethod read _OnNext write _OnNext;
+      property OnSend : tTiMethod read _OnSend write _OnSend;
+    end;
+
+
+implementation
+
+function tObj.eval( buf : tBuf ) : tBuf;
+  begin result := tBuf.Create(Nil);
   end;
 
-  { really there are four generic signatures }
-  tVoMeth = procedure of object;
-  tRuMeth = procedure ( buf : tBuf ) of object;
-  tNaMeth = function : tBuf of object;
-  tTiMeth = function ( buf : tBuf ) : tBuf of object;
-
-implementation
+procedure tObj.exec( buf : tBuf );
+  begin
+  end;
 
 begin
 end.
