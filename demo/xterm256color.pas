eea89f4db78be98b9a789f277b4692b78b3c2cf9
tangentstorm <michal.wallace@gmail.com>
2014-09-07 09:34:42 -0400

    cleaned up xt256 demo and added hexcode reference display

--- demo/xterm256color.pas
+++ demo/xterm256color.pas
@@ -2,13 +2,13 @@
 // Author: Todd Larason <jtl@molehill.org>
 // $XFree86: xc/programs/xterm/vttests/256colors2.pl,v 1.2 2002/03/26 01:46:43 dickey Exp $
 
-{$mode objfpc}{$i xpc.inc }
+{$mode delphiunicode}{$i xpc.inc }
 program xterm256color;
   uses xpc, kvm, ustr;
 
   procedure resetcolor;
   begin
-    write( #27, '[0m' )
+    fg('w'); bg('k');
   end;
 
   function hexbyte( b : byte ) : string;
@@ -28,29 +28,44 @@ program xterm256color;
     kvm.fg( 0 );
     write('-');
   end;
+
 
-  var red, green, blue, gray, color, level : byte;
+procedure section(msg : TStr);
+  begin
+    ResetColor;
+    Writeln; Writeln;
+    Writeln(msg);
+    Writeln; Writeln;
+  end;
 
+var
+  red, green, blue, gray, color, level, x,y : byte;
 begin
 
-  clrscr;
+  kvm.ClrScr;
 
-  { -- looks like he's configuring the palette here --------------------- }
+  { -- configure the palette --------------------- }
 
   // use the resources for colors 0-15 - usually more-or-less a
   // reproduction of the standard ANSI colors, but possibly more
-  // pleasing shades
+  // pleasing shades.
+
+  //  we have to write(stdout,#27), or else kvm strips out the escape! :(
+
   for red := 0 to 5 do
     for green := 0 to 5 do
       for blue := 0 to 5 do
       begin
-	write( #27, ']4;', 16 + (red * 36) + (green * 6) + blue, 'rgb:' );
-	if red <> 0 then write( hexbyte( red * 40 + 55 )) else write( '00' );
-	write('/');
-	if green <> 0 then write( hexbyte( green * 40 + 55 )) else write( '00' );
-	write('/');
-	if blue <> 0 then write( hexbyte( blue * 40 + 55 )) else write( '00' );
-	write( #27, '\' );
+	write( stdout, #27, ']4;', 16 + (red * 36) + (green * 6) + blue, 'rgb:' );
+	if red <> 0 then write( stdout, hexbyte( red * 40 + 55 ))
+	else write( stdout, '00' );
+	write(stdout, '/');
+	if green <> 0 then write(stdout, hexbyte( green * 40 + 55 ))
+	else write(stdout, '00' );
+	write(stdout, '/');
+	if blue <> 0 then write(stdout, hexbyte( blue * 40 + 55 ))
+	else write(stdout, '00' );
+	write(stdout, #27, '\' );
       end;
 
   // colors 232-255 are a grayscale ramp, intentionally leaving out
@@ -58,31 +73,24 @@ begin
   for gray := 0 to 23 do
   begin
     level := (gray * 10) + 8;
-    write( #27, ']4;', 232 + gray, 'rgb:' );
-    write( hexbyte( level ), '/', hexbyte( level ), '/', hexbyte( level ));
-    write( #27, '\' );
+    write( stdout, #27, ']4;', 232 + gray, 'rgb:' );
+    write( stdout, hexbyte( level ), '/', hexbyte( level ), '/', hexbyte( level ));
+    write( stdout, #27, '\' );
   end;
 
-
+
   // display the colors
-  { by drawing two space characters, with different background colors each time }
 
   // first the system ones:
-  writeln( 'System colors:');
-  writeln;
+  
+  section( 'System colors:');
   for color := 0 to 7 do showcolor( color );
-
-  resetcolor;
-  writeln;
-
+  resetcolor; writeln;
   for color := 8 to 15 do showcolor( color );
-  resetcolor;
-  writeln;
+  resetcolor;  writeln;
 
   // now the color cube
-  writeln;
-  writeln( 'Color cube, 6x6x6:' );
-  writeln;
+  section( 'Color cube, 6x6x6:' );
   for green := 0 to 5 do begin
     for red := 0 to 5 do begin
       for blue := 0 to 5 do begin
@@ -95,13 +103,22 @@ begin
   end;
 
   // now the grayscale ramp
-  writeln;
-  writeln( 'Grayscale ramp:' );
-  writeln;
+  section( 'Grayscale ramp:' );
   for color := 232 to 255 do showcolor( color );
 
-  resetcolor;
-  writeln;
-  writeln;
+  section( 'Hex Codes on Dark and Light backgrounds:' );
+  // now show the colors on dark and light backgrounds:
+  for y := 0 to 15 do begin
+    bg(0);
+    for x := 0 to 15 do begin
+      color := y*16+x; fg(color); write(hexbyte(color));
+    end;
+    write('  '); bg(15);
+    for x := 0 to 15 do begin
+      color := y*16+x; fg(color); write(hexbyte(color));
+    end;
+    writeln;
+  end;
 
+  resetcolor; writeln;
 end.
