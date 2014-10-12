22653860e375caa8ca842af6fc055e126ceab62a
tangentstorm <michal.wallace@gmail.com>
2014-10-12 01:43:43 -0500

    ucomb: ToStr, MoveNext, digits

--- code/ucomb.pas
+++ code/ucomb.pas
@@ -1,9 +1,10 @@
 {$mode delphi}{$i xpc}
 unit ucomb;
-interface uses xpc, num;
+interface uses xpc, num, math, sysutils;
 
 type
   TComboCursor = class (TObject)
+    _n, _k,
     _limit : cardinal;
     _bases,
     _digits : array of cardinal;
@@ -11,36 +12,89 @@ type
     constructor Create(n,k : cardinal); { k!n or "n choose k" }
     destructor Destroy; override;
     function GetBase( i : cardinal ): cardinal;
+    function GetDigit( i : cardinal ): cardinal;
     function GetLength : cardinal;
+    function GetValue : cardinal;
+    function ToStr:TStr;
+    procedure MoveNext;
     property bases[ i : cardinal ] : cardinal read GetBase;
+    property digit[ i : cardinal ] : cardinal read GetDigit;
     property limit : cardinal read _limit;
     property length : cardinal read GetLength;
+    property value : cardinal read GetValue;
+  end;
+
+implementation
+
+constructor TComboCursor.Create(n,k : cardinal);
+  var i,b : cardinal;
+  begin
+    inherited Create;
+    _limit := num.choose(n,k); _n := n; _k := k; b:=n-k+1;
+    SetLength(_bases,k);
+    SetLength(_digits,k);
+    if (k > 0) and (n > k) then
+      for i := 0 to k-1 do begin
+        // fractional bases
+        _bases[i] := b; inc(b);
+        _digits[i] := 0;
+      end
   end;
 
-implementation
+destructor TComboCursor.Destroy;
+  begin inherited
+  end;
+
 
 function TComboCursor.GetBase( i : cardinal ): cardinal;
   begin result := _bases[ i ]
+  end;
+
+function TComboCursor.GetDigit( i : cardinal ): cardinal;
+  begin result := _digits[ i ]
   end;
 
 function TComboCursor.GetLength : cardinal;
   begin result := system.Length(_bases)
   end;
 
-constructor TComboCursor.Create(n,k : cardinal);
+function TComboCursor.GetValue : cardinal;
+  var x : extended = 0; i : cardinal;
+  begin
+    writeln;
+    if _k = 0 then ok
+    else begin
+      x := _digits[_k-1];
+      if _k > 1 then
+	for i := _k-2 downto 0 do x +=  _digits[ i ] * (_k-i);
+    end;
+    result := ceil(x);
+  end;
+
+function TComboCursor.ToStr:TStr;
   var i : cardinal;
   begin
-    inherited Create;
-    _limit := num.choose(n,k);
-    SetLength(_bases,k); SetLength(_digits,k);
-    if (k > 0) and (n > k) then begin
-      for i := 0 to k-1 do _bases[i] := n-i;
-      for i := 0 to k-1 do _digits[i] := 0;
-    end
+    writestr(result, Format('TComboCursor(%d,%d):[ ', [_n,_k]));
+    for i := 0 to _k-1 do writestr(result, result, _digits[i], ' ');
+    writestr(result, result, ']');
   end;
 
-destructor TComboCursor.Destroy;
-  begin inherited
+procedure TComboCursor.MoveNext;
+  var i : cardinal; done : boolean;
+  begin
+    i := _k;  // start just after least signficant digit
+    if length > 0 then
+      repeat
+        dec(i);
+        inc(_digits[i]);
+        if (_digits[i] < _bases[i]) then
+          done := true
+        else begin
+          done := false;
+          _digits[i] := 0;
+          if i >0 then ok else i := _k; // TODO: mention carry?
+        end;
+      until done
   end;
 
 end.
--- test/test_ucomb.pas
+++ test/test_ucomb.pas
@@ -3,17 +3,48 @@ implementation uses ucomb;
 
 var cc : TComboCursor;
 
-procedure test_basics;
-  var count : cardinal;
+procedure setup;
   begin
+    if assigned(cc) then cc.Free;
     cc := TComboCursor.Create(52, 5); // 5 card poker hands
-    chk.equal( cc.limit, 2598960 );
-    chk.equal( cc.length, 5 );
-    chk.equal( cc.bases[ 0 ], 52 );
-    chk.equal( cc.bases[ 1 ], 51 );
-    chk.equal( cc.bases[ 2 ], 50 );
-    chk.equal( cc.bases[ 3 ], 49 );
-    chk.equal( cc.bases[ 4 ], 48 );
   end;
 
+procedure test_basics;
+  begin
+    chk.equal( cc.limit, 2598960 );
+    chk.equal( cc.length, 5 );
+    chk.equal( cc.bases[ 0 ], 48 ); chk.equal( cc.digit[ 0 ], 0 );
+    chk.equal( cc.bases[ 1 ], 49 ); chk.equal( cc.digit[ 1 ], 0 );
+    chk.equal( cc.bases[ 2 ], 50 ); chk.equal( cc.digit[ 2 ], 0 );
+    chk.equal( cc.bases[ 3 ], 51 ); chk.equal( cc.digit[ 3 ], 0 );
+    chk.equal( cc.bases[ 4 ], 52 ); chk.equal( cc.digit[ 4 ], 0 );
+  end;
+
+procedure test_counting;
+  begin
+    chk.equal( cc.value, 0 );
+    chk.equal( cc.digit[ 4 ], 0 );
+    cc.MoveNext;
+    chk.equal( cc.value, 1 );
+    chk.equal( cc.digit[ 4 ], 1 );
+  end;
+
+procedure test_count;
+  var i :integer;
+  begin
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 0 0 ]' );
+    cc.MoveNext;
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 0 1 ]' );
+    for i := 2 to 51 do cc.MoveNext;
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 0 51 ]' );
+    cc.MoveNext;
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 1 0 ]' );
+    cc.MoveNext;
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 1 1 ]' );
+    for i := 2 to 52 do cc.MoveNext;
+    chk.equal( cc.ToStr, 'TComboCursor(52,5):[ 0 0 0 2 0 ]' );
+  end;
+
+finalization
+  if assigned(cc) then cc.Free;
 end.
