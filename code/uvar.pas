61d667279fc09492f17603a9d2833e2167d16f68
tangentstorm <michal.wallace@gmail.com>
2014-12-07 00:53:10 -0600

    uvar: much improved support for arrays

--- code/uvar.pas
+++ code/uvar.pas
@@ -4,65 +4,123 @@ unit uvar;
 interface uses xpc, variants, sysutils;
 
 type
-  TVar = variant;
-  TVars = array of TVar;
+  TVar   = variant;
+  TVars  = variant; //array of TVar;
+  TVarFn = function(v : TVar):TVar;
 
 function A(vars : array of TVar) : TVars;
 
-function drop(n : word; vars: tvars) : TVars;
-function behead( vars : tvars ) :TVars;
-function implode(glue : tvar; vars: tvars) : TVars;
-function repr( v : TVar ) : TStr;
+function drop(n : word; vars: TVars) : TVars;
+function behead( vars : TVars ) :TVars;
+function link(x,y : TVar) : TVars;
+function implode(glue : TStr; vars: TVars) : TStr;
+function join(glue : TVar; vars: TVars) : TVars;
+function repr( v : TVar ) : TVar;
+function map(f : TVarFn; vars : TVars) : TVars;
+function len( v : TVar ) : cardinal;
 
+
 implementation
 
 {-- constructor for array of variants -------------------------}
 
-function A(vars : array of TVar) : TVars; inline;
-  begin result := g<TVar>.fromOpenArray(vars);
+function A(vars : array of TVar) : TVars;
+  var i : cardinal;
+  begin
+    if length(vars) > 0 then begin
+      result := VarArrayCreate([0,High(vars)],varVariant);
+      for i := 0 to High(vars) do result[i]:=vars[i];
+    end else result := NULL;
   end;
 
-{-- helpers ----------------------------------------------------}
+function len( v : TVar ) : cardinal;
+  begin
+    if varIsEmpty(v) or (v = NULL) or varIsNull(v) then result := 0
+      else if varIsArray(v) then
+        result:=varArrayHighBound(v,1)-varArrayLowBound(v,1)+1
+      else if varIsStr(v) then result:=length(v)
+      else result := 1
+  end;
+
+{-- restructuring tools ---------------------------------------}
 
 function drop(n : word; vars: tvars) : TVars; inline;
   var i : integer;
   begin
-    if length(vars) > n then begin
-      setlength(result, length(vars)-n);
-      for i := n to length(vars) -1 do result[i-n] := vars[i]
+    if vars.IsArray and (vars.ArrayHighBound > n) then begin
+      result := VarArrayCreate([0, vars.Count-n], vars.vType);
+      for i := n to vars.ArrayHighBound do result[i-n] := vars[i]
     end
-   else setlength(result,0)
+    else result := vars;
   end;
 
 function behead( vars : tvars ) :TVars; inline;
   begin result := drop(1, vars)
   end;
 
-function implode(glue : tvar; vars: tvars) : TVars; inline;
-  var i, len : integer;
+function link( x, y : TVar ): TVars;
+  var i,j : cardinal;
   begin
-    len := length(vars);
-    if len <= 1 then result := vars
+    if x = NULL then result := y
+    else if y = NULL then result := x
     else begin
-      setlength(result, len*2-1);
-      for i := 0 to len-2 do begin
-        result[i*2]:=vars[i]; result[i*2+1]:=glue
+      if not x.isArray then x := A([x]);
+      if not y.isArray then y := A([y]);
+      j := x.Count; result := VarArrayCreate([j+y.Count], varVariant);
+      if x.Count>0 then for i := 0 to x.ArrayHighBound do result[i] := x[i];
+      if y.Count>0 then for i := 0 to y.ArrayHighBound do result[j+i] := y[i];
+    end;
+  end;
+
+{--  string related --}
+function join(glue : tvar; vars: tvars) : TVars;
+  var i,vlen : integer;
+  begin vlen := len(vars);
+    case vlen of
+      0 : result := '';
+      1 : result := vars;
+      else begin
+        result := VarArrayCreate([0,vlen*2-2], varVariant);
+        for i := 0 to vlen-2 do begin
+          result[i*2]:=vars[i]; result[i*2+1]:=glue
+        end;
+        result[2*(vlen-1)] := vars[vlen-1]
       end;
-      result[2*(len-1)] := vars[len-1]
     end
   end;
 
-function repr( v : TVar ) : TStr; inline;
-  var item : TVar;
+function implode(glue : TStr; vars: tvars) : TStr;
+  var a:TVars; i:integer;
   begin
-    if VarIsArray(v) then
-      begin
-	result :='';
-	for item in implode(' ', TVars(v)) do result += repr(item);
-	if length(result) > 0 then result := '[ ' + result + ' ]'
-	else result := '[]'
-      end
-    else writestr(result, v);
+    a := join(glue,vars); result:='';
+    if len(a)=0 then ok
+    else if len(a)=1 then result := a[0]
+    else for i:=0 to len(a)-1 do result+=a[i];
+  end;
+
+function repr( v : TVar ) : TVar;
+  var s: TStr='';
+  begin
+    if v=NULL then s := '[]'
+    else if VarIsArray(v) then
+      s := implode(' ',A(['[',implode(' ',map(repr,v)),']']))
+    else if VarIsStr(v) then s := '"' + v + '"'
+    else writestr(s, v);
+    result := s;
+  end;
+
+
+{-- higher order functions --}
+
+function map(f : TVarFn; vars : TVars) : TVars;
+  var i,vlen : cardinal;
+  begin
+    vlen := len(vars);
+    if vlen=0 then result := A([])
+    else begin
+      result := VarArrayCreate([0,vlen-1], varVariant);
+      for i:=0 to vlen-1 do result[i] := f(vars[i]);
+    end
   end;
 
 initialization
--- test/test_uvar.pas
+++ test/test_uvar.pas
@@ -3,16 +3,16 @@ implementation uses uvar;
 
 procedure test_a;
   begin
-    chk.equal(0, length( A([]) ));
-    chk.equal(1, length( A([1]) ));
-    chk.equal(2, length( A([1,2]) ));
+    chk.equal(0, len( A([]) ));
+    chk.equal(1, len( A([1]) ));
+    chk.equal(2, len( A([1,2]) ));
   end;
 
 procedure test_implode;
   begin
-    chk.equal(0, length( implode( ' ', A([]) )));
-    chk.equal(1, length( implode( ' ', A(['1']) )));
-    chk.equal(3, length( implode( ' ', A(['1','2']) )));
+    chk.equal(0, len( implode( ' ', A([]) )));
+    chk.equal(1, len( implode( ' ', A(['1']) )));
+    chk.equal(3, len( implode( ' ', A(['1','2']) )));
   end;
 
 procedure test_repr;
